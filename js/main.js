c874e3e2a951a6321748df0c9dea91cc36971fb2
YUNINROK <inroktic@naver.com>
2020-02-07 16:39:33 +0900

    RELEASE

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 $(function(){
     $("#accordian .lnb_list a").click(function(){ //저걸 클릭하면
-        $(this).next("ul").slideUp(); //이거를 열어라
+        //var detph1font = "this"; //클릭 시 폰트 색상 변경 위한 변수
+
+        //$(this).addClass('lnb_selc');
+
+        $(this).next().slideUp(); //이거를 열어라
         if(!$(this).next().is(":visible")) // 만약 visible(보이는 상태)이 아니면
         {
             $(this).next().slideDown(); //닫아라
+        }
+        else{
+            //$(this).removeClass('lnb_selc')
+
         }
     });
 });
